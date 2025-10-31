4f7b79f251e41e2acc2f45921daeb9b7d75a9a63
koasare <koasare@live.com>
2025-10-31 13:37:49 -0500

    stable black window

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,9 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0,0,0))
+        pygame.display.flip()
+    pygame.quit()
 
 
 if __name__ == "__main__":
